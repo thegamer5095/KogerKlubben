9bb3ae9bf693b551626bc0fc13a61e30be00633b
thegamer5095 <84905478+thegamer5095@users.noreply.github.com>
2025-10-12 10:30:00 +0200

    DD

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -1,11 +1,37 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { Command } from '../../interfaces/Command';
+import {
+    SlashCommandBuilder,
+    PermissionFlagsBits,
+    EmbedBuilder,
+    ModalBuilder,
+    ActionRowBuilder,
+    LabelBuilder,
+    UserSelectMenuBuilder,
+  } from "discord.js";
+  import { Command } from "../../interfaces/Command";
 
 export const command: Command = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
     execute: async (interaction) => {
-        await interaction.reply('Pong!');
+        const modal = new ModalBuilder()
+    .setCustomId("test-modal")
+    .setTitle('Jeg nørder')
+
+
+    const selectMenu = new LabelBuilder()
+    .setLabel('User Select')
+    .setUserSelectMenuComponent(
+        new UserSelectMenuBuilder()
+        .setCustomId('test-select')
+        .setPlaceholder('Select a user')
+        .setMinValues(1)
+        .setMaxValues(2)
+    )
+
+
+    modal.addLabelComponents(selectMenu);
+
+    await interaction.showModal(modal);
     }
 }; 
