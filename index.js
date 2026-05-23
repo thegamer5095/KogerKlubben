574d30cb733a5716a5fc24624a79705dca1223a9
thegamer5095 <84905478+thegamer5095@users.noreply.github.com>
2026-05-23 13:32:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-// CommonJS bootstrap so hosts that run `node .` work with TypeScript.
 require("dotenv").config();
-require("ts-node/register");
-require("./src/index.ts");
 
+const fs = require("fs");
+const path = require("path");
+
+const distEntry = path.join(__dirname, "dist", "index.js");
+
+if (fs.existsSync(distEntry)) {
+  require(distEntry);
+} else {
+  require("ts-node/register");
+  require("./src/index.ts");
+}
